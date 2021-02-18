fc226a6a938dc4cbf192bcae6610098a1e5f54ec
Rasbats <miker@members.swis.net>
2021-02-18 12:08:08 +0000

    Align ShipDriver

--- cmake/PluginInstall.cmake
+++ cmake/PluginInstall.cmake
@@ -24,14 +24,11 @@ elseif (WIN32)
   message(STATUS "Install Prefix: ${CMAKE_INSTALL_PREFIX}")
   if (CMAKE_CROSSCOMPILING)
     install(TARGETS ${PACKAGE_NAME} RUNTIME DESTINATION "plugins")
-    set(INSTALL_DIRECTORY "plugins/${PACKAGE_NAME}")
   else ()
     install(TARGETS ${PACKAGE_NAME} RUNTIME DESTINATION "plugins")
-    set(INSTALL_DIRECTORY "plugins\\\\${PACKAGE_NAME}")
   endif ()
-
   if (EXISTS ${PROJECT_SOURCE_DIR}/data)
-    install(DIRECTORY data DESTINATION "${INSTALL_DIRECTORY}")
+    install(DIRECTORY data DESTINATION "plugins/${PACKAGE_NAME}")
   endif ()
 
 elseif (UNIX)
@@ -70,7 +67,8 @@ if (CMAKE_BUILD_TYPE MATCHES "Release|MinSizeRel")
     set(_striplib OpenCPN.app/Contents/PlugIns/lib${PACKAGE_NAME}.dylib)
   elseif (MINGW)
     set(_striplib plugins/lib${PACKAGE_NAME}.dll)
-  elseif (UNIX)  # linux
+  elseif (UNIX AND NOT CMAKE_CROSSCOMPILING AND NOT DEFINED ENV{FLATPAK_ID})
+    # Plain, native linux
     set(_striplib lib/opencpn/lib${PACKAGE_NAME}.so)
   endif ()
   if (BUILD_TYPE STREQUAL "tarball" AND DEFINED _striplib)
@@ -78,14 +76,12 @@ if (CMAKE_BUILD_TYPE MATCHES "Release|MinSizeRel")
     if (APPLE)
       set(STRIP_UTIL "${STRIP_UTIL} -x")
     endif ()
-    install(CODE
-      "execute_process(COMMAND cmake -E echo Stripping ${_striplib})"
-    )
-    install(CODE
-      "execute_process(
+    install(CODE "message(STATUS \"Stripping ${_striplib}\")")
+    install(CODE "
+      execute_process(
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMAND ${STRIP_UTIL} app/files/${_striplib}
-      )"
-    )
+      )
+    ")
   endif ()
 endif ()
